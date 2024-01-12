30b58eed11769ef837f580b7eb8921aff6132bc4
Miguel Briceño <miguel.briceno@evertecinc.com>
2024-01-12 14:32:57 -0500

    Spectral style file deleted

--- .spectral.mjs
+++
@@ -1,2 +0,0 @@
-import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjIzMTAzNg/spectral.js?branch_slug=main&token=858d7f96-c181-46bc-bdb9-bdcedcf60c9e";
-export default { extends: ruleset };
