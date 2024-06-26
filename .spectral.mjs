feb345c9d7ebdf014f2d38274df7a493e22c527d
lauana.oliveira@evertecinc.com <lauana.oliveira@evertecinc.com>
2024-06-26 11:43:11 -0500

    Modified 2 files

--- .spectral.mjs
+++
@@ -1,2 +0,0 @@
-import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjE5NjY4OA/spectral.js?branch_slug=feature%2Fdoc-iit&token=4db6203b-027b-4c8a-8a18-ab79284ec97c";
-export default { extends: ruleset };
